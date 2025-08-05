b5ac33f2cb0bf07851f0f440741bd389aebc8c0a
Anders Blair Liu <andersliu@berkeley.edu>
2025-08-05 01:01:11 +0000

    game working locally and all tests passing spec

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -3,11 +3,47 @@ class WordGuesserGame
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  attr_accessor :word, :guesses, :wrong_guesses
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter.strip.empty? || letter !~ /^[a-zA-Z]$/
+
+    letter.downcase!
+    return false if (@guesses + @wrong_guesses).include?(letter)
+
+    if @word.include?(letter)
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+
+    true
   end
 
+  def word_with_guesses
+    w = ''
+    @word.each_char do |c|
+      w += @guesses.include?(c) ? c : '-'
+    end
+    w
+  end
+
+  def check_win_or_lose
+    if word_with_guesses == @word
+      :win
+    elsif @wrong_guesses.length >= 7
+      :lose
+    else
+      :play
+    end
+  end
+  
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
